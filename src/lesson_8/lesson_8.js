dbd80af433bfb1b2fdeb367714bc9aac63bb9f43
Natalizag <55549023+Natalizag@users.noreply.github.com>
2019-10-02 16:53:55 +0300

    init repo

---
+++ src/lesson_8/lesson_8.js
@@ -0,0 +1,6 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
+library.add(faCheck);
+dom.watch();
+
+import './lesson_8.scss';
